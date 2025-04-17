61442e38c58e534bb5b1775cbd67bd458a1fd24f
rjonathan87 <rjonathan87@gmail.com>
2025-04-17 16:09:59 -0600

    subiendo ca

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5c3b7fa44da8a8ea"; }
+/******/ 		__webpack_require__.h = function() { return "a496d047c6f5480b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
